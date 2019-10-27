be2f6f7acb43472fad2892f883fea44bea984768
Sergey <kra1d@yandex.ru>
2019-10-27 23:07:39 +0300

    Change game code

--- number_gues.py
+++ number_gues.py
@@ -1,29 +1,14 @@
 # Угадай число
 import random
 
+answer = random.randint(1,101)
+guess = 0
+while guess != answer:
+    guess = int(input('Input your number: '))
+    if guess < answer:
+        print('Greater')
+    elif guess > answer:
+        print('Lower')
+    else:
+        print('You are win!!!')
 
-# Компьютер загадывает число в пределах от 1 до 100 включительно
-def what_number():
-    return random.randrange(1, 101)
-
-
-def user_inp():
-    user_input = int(input('Input your number: '))
-    return user_input
-
-
-def game():
-    comp_number = what_number()
-    user_number = user_inp()
-    while user_number != comp_number:
-        if user_number > comp_number:
-            print('Nope. Lower.')
-        elif user_number < comp_number:
-            print('Greater')
-        user_number = user_inp()
-    print('You are win!!!')
-    print("Let's play again")
-    game()
-
-
-game()
